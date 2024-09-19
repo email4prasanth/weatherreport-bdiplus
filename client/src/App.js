f559a36ec3f26b157e1f788ad8b63ce125335df6
email4prasanth <email4prasanth@gmail.com>
2024-09-19 12:09:02 +0530

    modifies app.js

--- client/src/App.js
+++ client/src/App.js
@@ -7,8 +7,8 @@ function App() {
     <div>
       {/* <Form/> */}
       <Routes>
-      {/* <Route path="/" element = {<Form/>}/> */}
-      <Route path={"/weather"} element = {<Form/>}/>
+      <Route path="/" element = {<Form/>}/>
+      {/* <Route path={"/weather"} element = {<Form/>}/> */}
       <Route path ={'/weatherinfo'} element={<WeatherData/>} />
       </Routes>
     </div>
